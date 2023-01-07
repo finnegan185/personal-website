40b0885f90f88629eae099a54c6ab96761a86319
Zack Finnegan <zack.finnegan4@gmail.com>
2023-01-06 21:12:22 -0500

    updated zack university url

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -6,7 +6,7 @@ function Portfolio(props) {
     <div className="container-lg portfolio-container">
       <h3 className="text-center">My Portfolio</h3>
       <div className="d-flex flex-wrap justify-content-evenly">
-        <Card src="\public\ZackUniversityCardSm.JPG" alt="ZackUniversity" title="Oncology Research Web App" link="https://zfinnegan.com/" />
+        <Card src="\public\ZackUniversityCardSm.JPG" alt="ZackUniversity" title="Zack University" link="https://university.zfinnegan.com/" />
 
         <Card src="\public\SocialMediaAppReactSm.JPG" alt="React Course" title="React Course Web App" link="https://reactcourse.zfinnegan.com/" />
 
